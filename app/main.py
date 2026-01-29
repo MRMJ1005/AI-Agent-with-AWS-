1a617933d50099d117f83a930d57b00dd878a32b
Syed Mazhar <syedmazharhussain2607@gmail.com>
2026-01-29 16:01:08 +0530

    project completed

---
+++ app/main.py
@@ -0,0 +1,35 @@
+import subprocess
+import threading 
+import time 
+
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+from dotenv import load_dotenv
+
+load_dotenv()
+logger=get_logger(__name__)
+
+def run_backend():
+    try:
+        logger.info("starting the backend service ...")
+        subprocess.run(["uvicorn","app.backend.api:app","--host", "127.0.0.1", "--port", "9999"],check=True)
+    except CustomException as e:
+        logger.error("Problem with starting the backend")
+        raise CustomException("Failed to start backend ", e)
+
+def run_frontend():
+    try:
+        logger.info("starting the frontend service ..")
+        subprocess.run(["streamlit","run", "app/frontend/ui.py"], check=True)
+    except CustomException as e:
+        logger.error("Problem with frontend service ..")
+        raise CustomException("Failed to start Frontend ", e)
+    
+if __name__=="__main__":
+    try:
+        threading.Thread(target=run_backend).start()
+        time.sleep(2)
+        run_frontend()
+    except CustomException as e:
+        logger.exception(f"CustomException occured : , {str(e)}")
+        
